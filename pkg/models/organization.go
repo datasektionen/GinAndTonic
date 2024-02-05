aa121eb3cb5800099cb61c645cb98a20b3cf8465
Lucas Dow <lucdow7@gmail.com>
2024-02-05 10:50:19 +0100

    Update organization name validation regex

--- pkg/models/organization.go
+++ pkg/models/organization.go
@@ -39,7 +39,7 @@ func (o Organization) ValidateName() error {
 	if len(o.Name) > 50 {
 		return errors.New("name must not be longer than 50 characters")
 	}
-	if match, _ := regexp.MatchString(`^[a-zA-Z0-9]*$`, o.Name); !match {
+	if match, _ := regexp.MatchString(`^[\p{L}\p{N}]*$`, o.Name); !match {
 		return errors.New("name must not contain special characters or spaces")
 	}
 	return nil
